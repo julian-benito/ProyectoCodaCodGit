928a9f4d0a0dec7cb460c51bd96b4b3a6dc5e9e0
Julian Benito <julianbenito5@gmail.com>
2024-05-06 19:10:31 -0300

    seccion nosotros actualizacion

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/* NAVBAR RESPONSIVE*/ 
 const navLinks = document.querySelector('.nav__links');
 const navIcons = document.querySelector('.nav__icons');
 const abrir = document.querySelector('#abrir');
@@ -10,7 +11,27 @@ abrir.addEventListener('click', () => {
 });
 
 cerrar.addEventListener('click', () => {
-    navLinks.classList.remove('nav__visible');
-    navIcons.classList.remove('nav__visible');
-    abrir.style.display = 'block';
-  });
+  navLinks.classList.remove('nav__visible');
+  navIcons.classList.remove('nav__visible');
+  abrir.style.display = 'block';
+});
+
+/*ANIMACION TEXTOS NOSOTROS*/
+
+const texts = document.querySelectorAll('.text__animation');
+
+window.addEventListener('scroll', checkTexts);
+
+function checkTexts(){
+  const triggerTexts = window.innerHeight / 5 * 4
+
+  texts.forEach(text => {
+    const textTop = text.getBoundingClientRect().top
+
+    if(textTop < triggerTexts){
+      text.classList.add('show')
+    } else {
+      text.classList.remove('show')
+    }
+  })
+}
